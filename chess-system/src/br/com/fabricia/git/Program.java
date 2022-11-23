1966882c692aca73ae2070944c9176ed07b1c19b
fabricia1981 <86792795+fabricia1981@users.noreply.github.com>
2022-11-23 09:21:45 -0300

    Adicionei arquivos

---
+++ chess-system/src/br/com/fabricia/git/Program.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package br.com.fabricia.git;
+
+/**
+ * @author FGALLO
+ *
+ */
+public class Program {
+
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello Git");
+
+	}
+
+}
